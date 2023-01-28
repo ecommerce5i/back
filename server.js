e65020e8efb6dae343f7ddcb681075c25577e0d8
Alfredo A. Correa <105443143+acorrea15@users.noreply.github.com>
2023-01-28 02:52:39 -0300

    add

--- server.js
+++ server.js
@@ -47,8 +47,11 @@ app.post('/create-payment', async(req, res)=> {
 })
 
 
-server.listen(8080, ()=> {
-  console.log('Servidor corriendo en el puerto', 1111)
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+/* server.listen(8080, ()=> {
+  console.log('Servidor corriendo en el puerto', 8080)
+}) */
 
 app.set('socketio', io);
